026fa75f01430e8d8a08805913493bf7d14eae65
shinesanthosh <shinesanthosh2000@gmail.com>
2022-03-21 21:38:45 +0530

    Update navbar to accept another styling theme
    The website uses 2 different color themes for navbar on different pages
    A unique one for hompepage and a common one for others
    The Menu component (navbar) now accepts a prop named mode, for this
    setting mode to anything other than 2 will result in default theme
    default theme is used for home page only
    Other pages use another theme which can be applied by setting mode = 2

--- components/menu.js
+++ components/menu.js
@@ -1,34 +1,60 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 
 import styles from '../styles/Menu.module.css'
 
-const Menu = (props) => {
-
+const Menu = props => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <nav className={styles.menuContainer}>
-      <Link href='/'>
-      <img className={isOpen ? '':styles.logo} src='logo.png' alt='Logo' /></Link>
-      <img className={styles.ham} src={isOpen ?'/close.png': '/hamburger.png'} onClick={()=>{setIsOpen(!isOpen)}}/>
-      <ul className={isOpen?`${styles.navopen} ${styles.navul}`:styles.navul}>
+      <Link href="/">
+        <img className={isOpen ? '' : styles.logo} src="logo.png" alt="Logo" />
+      </Link>
+      <img
+        className={styles.ham}
+        src={isOpen ? '/close.png' : '/hamburger.png'}
+        onClick={() => {
+          setIsOpen(!isOpen)
+        }}
+      />
+      <ul
+        className={isOpen ? `${styles.navopen} ${styles.navul}` : styles.navul}
+      >
         <li>
-          <Link href='/about'><a className={styles.navbtn}>ABOUT US</a></Link>
-        </li>
-        <li>
-          <Link href='/products'><a className={styles.navbtn}>PRODUCTS</a></Link>
-        </li>
-        <li>
-          <Link href='/blog'><a className={styles.navbtn}>BLOG</a></Link>
-        </li>
-        <li>
-          <Link href='/register'>
-            <a className={styles.regbtn}>REGISTER</a>
+          <Link href="/about">
+            <a className={props.mode != 2 ? styles.navbtn : styles.navbtn2}>
+              ABOUT US
+            </a>
           </Link>
         </li>
         <li>
-          <Link href='/login'><a className={styles.navbtn}>LOGIN</a></Link>
+          <Link href="/products">
+            <a className={props.mode != 2 ? styles.navbtn : styles.navbtn2}>
+              PRODUCTS
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <a className={props.mode != 2 ? styles.navbtn : styles.navbtn2}>
+              BLOG
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/register">
+            <a className={props.mode != 2 ? styles.regbtn : styles.regbtn2}>
+              REGISTER
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/login">
+            <a className={props.mode != 2 ? styles.navbtn : styles.navbtn2}>
+              LOGIN
+            </a>
+          </Link>
         </li>
       </ul>
     </nav>
