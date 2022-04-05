95295cae735ff4f5942367526712944cc4158c44
shinesanthosh <shinesanthosh2000@gmail.com>
2022-04-06 03:00:56 +0530

    Style blogs page
    The blogs page is styled
    Not responsive

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from '../../styles/Blog.module.css'
 import Link from 'next/link'
+import Menu from '../../components/menu'
+import Footer from '../../components/footer'
 
 export const getStaticProps = async () => {
   const url = process.env.CMSURL
@@ -15,168 +17,38 @@ export const getStaticProps = async () => {
 
 const Blog = ({ data }) => {
   return (
-    <>
-      <div className={styles.top}>
-        <img className={styles.logo} src='Group 243.png' />
-        <ul className={styles.ul}>
-          <li>
-            <Link href='/about'>ABOUT US</Link>
-          </li>
-          <li>
-            <Link href='/products'>PRODUCTS</Link>
-          </li>
-          <li>
-            <Link href='/blog'>BLOG</Link>
-          </li>
-          <li>
-            <button className={styles.reg}>
-              <Link href='/register'>REGISTER</Link>
-            </button>
-          </li>
-          <li>
-            <Link href='/login'>LOG IN</Link>
-          </li>
-        </ul>
+    <div className={styles.blog}>
+      <Menu mode={2} />
 
-        <br />
-        <br />
-
-        <div className={styles.topsearch}>
-          <h1 className={styles.gg}>Journal</h1>
-          <form className={styles.searchbar}>
-            <div>
-              <input
-                className={styles.kk}
-                type='search'
-                placeholder='search....'
-              />{' '}
-              <button type='submit' className={styles.mm}>
-                GO
-              </button>
-            </div>
-          </form>{' '}
-        </div>
+      <div className={styles.titleContainer}>
+        <h1>Journal</h1>
+        <form className={styles.searchbar}>
+          <input type="text" placeholder="Search...." />
+          <input type="submit" value="GO" />
+        </form>
       </div>
-      <br></br>
-
-      {data.map((blog, key) => {
-        return (
-          <div className={styles.cards1} key={key}>
-            <div className={styles.image}>
+      <div className={styles.postsContainer}>
+        {data.map((blog, key) => {
+          return (
+            <div className={styles.post} key={key}>
               <img src={blog.mainImage.asset.url} />
+              <div className={styles.textContent}>
+                <h3>{blog.title}</h3>
+
+                <p className={styles.postdesc}>
+                  {blog.body[0].children[0].text}
+                </p>
+
+                <Link href={'/blog/' + blog.slug.current}>
+                  <a className={styles.posturl}> READ MORE</a>
+                </Link>
+              </div>
             </div>
-            <br />
-            <h3>{blog.title}</h3>
-            <br />
-            <p className={styles.p}>{blog.body[0].children[0].text}</p>
-            <br />
-            <button>
-              <Link href={'/blog/' + blog.slug.current}>READ MORE</Link>
-            </button>
-          </div>
-        )
-      })}
-
-      <div className={styles.bot}>
-        <ul className={`${styles.ul} ${styles.botul}`} >
-          <br />
-          <li>
-            <strong>Information</strong>
-          </li>
-          <br />
-          <br />
-          <li>
-            <a href='#'>Home</a>
-          </li>
-          <br />
-          <li>
-            <a href='#'>Services</a>
-          </li>
-          <br />
-          <li>
-            <a href='#'>Solution</a>
-          </li>
-          <br />
-          <li>
-            <a href='#'>FAQ</a>
-          </li>
-          <br />
-          <li>
-            <a href='#'>Privacy Policy</a>
-          </li>
-        </ul>
-        <ul className={`${styles.ul} ${styles.Com}`} >
-          <br />
-          <li>
-            <strong>Company</strong>
-          </li>
-          <br />
-          <br />
-          <li>
-            <a href='#'> About Us</a>
-          </li>
-          <br />
-          <li>
-            <a href='#'>Contact Us</a>
-          </li>
-          <br />
-          <li>
-            <a href='#'>Journal</a>
-          </li>
-          <br />
-          <li>
-            <a href='#'>Register</a>
-          </li>
-          <br />
-          <li>
-            <a href='#'>Login</a>
-          </li>
-          <br />
-        </ul>
+          )
+        })}
       </div>
-
-      <div className={styles.mid}>
-        <br />
-        <br />
-        <h3 className={styles.head}>pradjna</h3>
-        <br />
-        <p className={`${styles.p} ${styles.pp}`} >
-          Pradjna is a cutting edge B2B HR performance
-          <br /> management platform focused on re-
-          <br />
-          engineering legacy business processes using <br />
-          Machine Learning.
-        </p>
-        <br />
-        <a href='#'>
-          <img src='Ellipse 26.png' />
-        </a>
-        <a href='#'>
-          <img className={styles.twit} src='Ellipse 25.png' />
-        </a>
-        <a href='#'>
-          <img className={styles.insta} src='Ellipse 27.png' />
-        </a>
-        <br />
-      </div>
-      <div className={styles.lasti}>
-        <h4 className={styles.gett}>Get news and updates!</h4>
-        <br />
-        <p className={styles.p}>Learn about growing your business.</p>
-        <br />
-        <div className={styles.subii}>
-          <input className={styles.subi} type='search' id='quary' />
-          <button className={styles.aaa} value='submit'>
-            SUBSCRIBE
-          </button>
-        </div>
-      </div>
-      <br />
-      <p className={`${styles.p} ${styles.copyi}`} >
-        {' '}
-        Copyright © Pradjna Intellisys
-      </p>
-    </>
+      <Footer />
+    </div>
   )
 }
 
