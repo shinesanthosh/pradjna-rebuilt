36f82947ff7889b392cfa4182345f262ac0162c0
shinesanthosh <shinesanthosh2000@gmail.com>
2022-04-17 02:46:39 +0530

    Add image slider to the top of about page
    A manual image slider to the about page
    Added as seperate component
    Image doesn't change automatically
    Buttons can control the image and caption being displayed
    The icons/text inside the button needs to be modified to match UI design
    Not Responive

---
+++ components/aboutSlider.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import styles from '../styles/Slider.module.css'
+
+const Slider = ({ data }) => {
+  const [pos, setPos] = useState(0)
+
+  const maxPos = data.length - 1
+  const posChangeHandler = operation => {
+    if (operation) setPos(pos + 1 > maxPos ? 0 : pos + 1)
+    else setPos(pos - 1 < 0 ? maxPos : pos - 1)
+  }
+
+  let dotArray = []
+  for (let i = 0; i <= maxPos; i++)
+    dotArray.push(
+      <div className={`${styles.dot} ${i == pos ? styles.dotActive : ''}`} key={i} >
+        {' '}
+      </div>
+    )
+  return (
+    <div className={styles.slider}>
+      <img src={data[pos].image.asset.url} />
+      <div className={styles.textNControl}>
+        <div className={styles.captions}>
+          <h5>{data[pos].caption}</h5>
+          <p>{data[pos].description}</p>
+        </div>
+        <div className={styles.sliderControl}>
+          <div className={styles.controls}>
+            <div
+              className={`${styles.prevBtn} ${styles.button} ${
+                pos != 0 ? '' : styles.disBtn
+              }`}
+              onClick={() => {
+                posChangeHandler(false)
+              }}
+            >
+              &lt;
+            </div>
+
+            <div
+              className={`${styles.nextBtn} ${
+                pos >= maxPos ? styles.disBtn : ''
+              } ${styles.button}`}
+              onClick={() => {
+                posChangeHandler(true)
+              }}
+            >
+              &gt;
+            </div>
+          </div>
+          <div className={styles.dotContainer}>{dotArray}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Slider
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Menu from '../components/menu'
+import Slider from '../components/aboutSlider'
 
 import styles from '../styles/About.module.css'
 
@@ -10,35 +11,21 @@ export const getStaticProps = async () => {
   let members, faqs, titleSlider
 
   await fetch(url + '?query=' + encodeURIComponent(query))
-    .then((res) => res.json())
-    .then((data) => {
+    .then(res => res.json())
+    .then(data => {
       members = data.result[0].members
       faqs = data.result[0].faqs
       titleSlider = data.result[0].titleSlider
     })
 
-  return { props: { members, faqs, titleSlider} }
+  return { props: { members, faqs, titleSlider } }
 }
 
-const About = ({ members, faqs, titleSlider}) => {
+const About = ({ members, faqs, titleSlider }) => {
   return (
-    <div className={styles.wrapper}>
-      <img className={styles.logo} src='logo.png' />
-      <Menu btnclass={styles.btnji} />
-      <div className={styles.slider}>
-        <figure>
-          {titleSlider.map((slide,key)=><div key={key}>
-            <img className={styles.ttt} src={slide.image.asset.url} />
-            <h5>{slide.caption}</h5>
-            <p>{slide.description}</p>
-          </div>)}
-          
-          
-        </figure>
-      </div>
-      <br />
-      <br />
-      <br />
+    <div className={styles.about}>
+      <Menu mode={2} />
+      <Slider data={titleSlider} />
       <strong>
         {' '}
         <h1 className={styles.ggy}>About Us</h1>
@@ -67,7 +54,7 @@ const About = ({ members, faqs, titleSlider}) => {
         <div key={key}>
           <p className={styles.hjk}>{faq.qn}</p>{' '}
           <p className={styles.mmm}>{faq.ans}</p>
-          <img className={styles.im} src='Group 134.png' />
+          <img className={styles.im} src="Group 134.png" />
         </div>
       ))}
       <h1 className={styles.zz1}></h1>
@@ -97,9 +84,9 @@ const About = ({ members, faqs, titleSlider}) => {
       <br />
       <div>
         &#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;&#8200;{' '}
-        <img className={styles.abm} src='Group 178.png' />
-        <img className={styles.abm} src='Group 178.png' />
-        <img className={styles.abm} src='Group 178.png' />{' '}
+        <img className={styles.abm} src="Group 178.png" />
+        <img className={styles.abm} src="Group 178.png" />
+        <img className={styles.abm} src="Group 178.png" />{' '}
       </div>
       <br />
       <br />
@@ -109,26 +96,26 @@ const About = ({ members, faqs, titleSlider}) => {
       <br />
       <br />
       <label className={`${styles.ss} ${styles.label}`}>Name</label>
-      <input className={styles.input} type='email' id='email' size='30' />
+      <input className={styles.input} type="email" id="email" size="30" />
       <br />
       <br />
       <br />
       <label className={`${styles.ss} ${styles.label}`}>Email</label>
-      <input className={styles.input} type='email' id='email' size='30' />
+      <input className={styles.input} type="email" id="email" size="30" />
       <br />
       <br />
       <br />
       <label className={`${styles.ssg} ${styles.label}`}>Message</label>
       <input
         className={`${styles.input} ${styles.sss}`}
-        type='email'
-        id='email'
-        size='30'
+        type="email"
+        id="email"
+        size="30"
       />{' '}
       <br />
       <br />
       <br />
-      <input className={styles.input} type='submit' value='Send' />
+      <input className={styles.input} type="submit" value="Send" />
       <h1 className={styles.ere}>Visit Us</h1>
       <br />
       <br />
@@ -147,12 +134,19 @@ const About = ({ members, faqs, titleSlider}) => {
         695581
       </p>
       <div className={styles.contactmap}>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d878.7380009011521!2d76.90230792691472!3d8.557436889531646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05bedb3f2eaaab%3A0xe85b8f58f4029682!2sPradjna%20Intellisys!5e0!3m2!1sen!2sin!4v1642671054215!5m2!1sen!2sin" width="600" height="450" styles={{border:0}} allowFullScreen="" loading="lazy"></iframe>
-    </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d878.7380009011521!2d76.90230792691472!3d8.557436889531646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05bedb3f2eaaab%3A0xe85b8f58f4029682!2sPradjna%20Intellisys!5e0!3m2!1sen!2sin!4v1642671054215!5m2!1sen!2sin"
+          width="600"
+          height="450"
+          styles={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
       <br />
       <br />
       <br />
-      <img className={styles.lstimg} src='Group 182.png' />
+      <img className={styles.lstimg} src="Group 182.png" />
       <br />
       <br />
       <br />
@@ -206,9 +200,9 @@ const About = ({ members, faqs, titleSlider}) => {
           Machine Learning.
         </p>
         <br />
-        <img src='Ellipse 26.png' />
-        <img src='Ellipse 25.png' />
-        <img src='Ellipse 27.png' />
+        <img src="Ellipse 26.png" />
+        <img src="Ellipse 25.png" />
+        <img src="Ellipse 27.png" />
         <br />
       </div>
       <div className={styles.lasti}>
@@ -217,8 +211,8 @@ const About = ({ members, faqs, titleSlider}) => {
         <p>Learn about growing your business.</p>
         <br />
         <div className={styles.subii}>
-          <input className={styles.subi} type='search' id='quary' />
-          <button className={styles.aaa} value='submit'>
+          <input className={styles.subi} type="search" id="quary" />
+          <button className={styles.aaa} value="submit">
             SUBSCRIBE
           </button>
         </div>
