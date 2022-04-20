6818f07556a7bfb99798e2bbb64c0b24c8aa86ca
shinesanthosh <shinesanthosh2000@gmail.com>
2022-04-21 02:52:31 +0530

    Style product page for individual products
    The page that describes products individually
    Laid out and styled
    Not responsive
    Need refinements

--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from '../../styles/Product.module.css'
+
 import Link from 'next/link'
 import BlockContent from '@sanity/block-content-to-react'
+import Menu from '../../components/menu'
+import Footer from '../../components/footer'
 
 export const getStaticPaths = async () => {
   const url = process.env.CMSURL
@@ -24,7 +27,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params }) => {
   const url = process.env.CMSURL
-  const query = `*[_type == 'product']{title,image{asset->{url}},features,description,body[]{_type,children,style,asset->{url}},useCases[]->{title,slug{current}}}`
+  const query = `*[_type == 'product' && id.current == '${params.product}']{title,image{asset->{url}},features,description,body[]{_type,children,style,asset->{url}},useCases[]->{title,slug{current}}}`
 
   const res = await fetch(url + '?query=' + encodeURIComponent(query))
   const data = await res.json()
@@ -43,126 +46,47 @@ export const getStaticProps = async ({ params }) => {
 
 const Product = ({ title, image, features, description, body, useCases }) => {
   return (
-    <div className={styles.wrapper}>
-      <img className={styles.logo} src='/logo.png' />
-      <ul>
-        <li>
-          <Link href='/about'>ABOUT US</Link>
-        </li>
-        <li>
-          <Link href='/products'>PRODUCTS</Link>
-        </li>
-        <li>
-          <Link href='/blog'>BLOG</Link>
-        </li>
-        <li>
-          <button className={styles.btn}>REGISTER</button>
-        </li>
-        <li>
-          <Link href='/login'>LOGIN</Link>
-        </li>
-      </ul>
-      <h1 className={styles.ab}>{title}</h1>
-      <p className={styles.para}>{description}</p>
-      <br /> <img className={styles.blue} src={image.asset.url} />
-      <br /> <br />
-      <h2 className={styles.fe}> &#02008;Features </h2>
-      <br />
-      {features.map((feature, key) => (
-        <div key={key}>
-          <img className={styles.list} src='/list.png' />
-          <p className={styles.line}>{feature}</p>
-        </div>
-      ))}
-      <br /> <br />{' '}
-      <p className={styles.screen}>
+    <div className={styles.product}>
+      <Menu mode={2} />
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.titleDesc}>{description}</p>
+      <img className={styles.mainImage} src={image.asset.url} />
+
+      <div className={styles.features}>
+        <h2> Features </h2>
+        <ul className={styles.featureList}>
+          {features.map((feature, key) => (
+            <li key={key}>
+              <img src="/list.png" />
+              <p>{feature}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <p className={styles.content}>
         <BlockContent blocks={body} />
       </p>
-      <br />
-      <h7 className={styles.sub}>Usecases</h7>
-      <br />
-      &#02008;{' '}
 
-      {
-        useCases.map((useCase,key) => 
-        <button className={styles.vtl3} key={key}>
-        {useCase.title}
-        <br />
-        &#02008;
-        <Link className={styles.learn} href={`/products/usecases/${useCase.slug.current}`}>
-          LEARN MORE &gt;
-        </Link>{' '}
-      </button>
-        )
-      }
-      
-      
-      <br />
-      <div className={styles.bot}>
-        <ul>
-          <br />
-          <li>
-            <strong>Information</strong>
-          </li>
-          <br />
-          <li>Home</li>
-          <br />
-          <li>Services</li>
-          <br />
-          <li>Solution</li>
-          <br />
-          <li>FAQ</li>
-          <br />
-          <li>Privacy Policy</li>
-        </ul>
-        <ul className={styles.Com}>
-          <br />
-          <li>
-            <strong>Company</strong>
-          </li>
-          <br />
-          <li>About Us</li>
-          <br />
-          <li>Contact Us</li>
-          <br />
-          <li>Journal</li>
-          <br />
-          <li>Register</li>
-          <br />
-          <li>Login</li>
-          <br />
-        </ul>
-      </div>
-      <div className={styles.mid}>
-        <br />
-        <h3 className={styles.head}>pradjna</h3>
-        <br />
-        <p className={styles.pp}>
-          Pradjna is a cutting edge B2B HR performance
-          <br /> management platform focused on re-
-          <br />
-          engineering legacy business processes using <br />
-          Machine Learning.
-        </p>
-        <br />
-        <img src='/Ellipse 26.png' />
-        <img src='/Ellipse 25.png' />
-        <img src='/Ellipse 27.png' />
-        <br />
-      </div>
-      <div className={styles.lasti}>
-        <h4 className={styles.gett}>Get news and updates!</h4>
-        <br />
-        <p>Learn about growing your business.</p>
-        <br />
-        <div className={styles.subii}>
-          <input className={styles.subi} type='search' id='quary' />
-          <button className={styles.aaa} value='submit'>
-            SUBSCRIBE
-          </button>
+      <div className={styles.useCases}>
+        <h3>Usecases</h3>
+
+        <div className={styles.useCasesList}>
+          {useCases.map((useCase, key) => (
+            <div className={styles.useCase} key={key}>
+              <span>{useCase.title}</span>
+
+              <Link
+                className={styles.learn}
+                href={`/products/usecases/${useCase.slug.current}`}
+              >
+                LEARN MORE &gt;
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
-      <p className={styles.copyi}> Copyright © Pradjna Intellisys</p>
+      <Footer />
     </div>
   )
 }
